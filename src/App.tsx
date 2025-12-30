f9b41dc1855a143d56fee8fd0061b7b0f88b9b49
Malmeu <darkvid3r@gmail.com>
2025-12-30 21:17:54 +0100

    fix: ajout ScrollToTop pour afficher le haut de la page lors de la navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import ProductDetailOneStep from './pages/ProductDetailOneStep'
@@ -64,6 +65,7 @@ function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar cartItemsCount={cartItemsCount} />
         <main className="flex-1">
